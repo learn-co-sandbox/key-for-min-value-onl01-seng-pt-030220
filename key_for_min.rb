2cd1f857ddde0180a128378ee61afb4c095fdd8d
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-03-13 02:16:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,46 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  smallest_name = nil
+  smallest_value = nil
+  name_hash.each do |name, num|
+  if smallest_value == nil
+      smallest_name = name 
+      smallest_value = num
+    elsif num < smallest_value 
+      num = smallest_value
+      smallest_name = name
+    end
+  end
+  smallest_name
+  end
+#   mk = nil
 
-end
+# return_key = nil
+
+# name_hash.each do |key, a_value|
+
+#   if mk == nil
+
+#     mk = a_value
+
+#     return_key = key
+
+# else
+
+#   if a_value < mk
+
+ 
+#   mk = a_value
+ 
+#   return_key = key
+
+# end
+
+# end
+
+# end
+
+# return_key
+
+# end
